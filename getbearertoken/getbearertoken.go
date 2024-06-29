c5671cc799ecd19a6e73ee082d9ef92ee960e09a
Paulo <pmarques@microsoft.com>
2024-06-29 00:17:52 +0000

    adding support for managed identity

--- getbearertoken/getbearertoken.go
+++ getbearertoken/getbearertoken.go
@@ -24,6 +24,7 @@ import (
 
 	"getbearertoken/internal/utils"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"software.sslmate.com/src/go-pkcs12"
@@ -35,21 +36,23 @@ const (
 	ERR_INVALID_ARGUMENT      = 4
 	ERR_CERTIFICATE_NOT_FOUND = 5
 	ERR_CERTIFICATE           = 6
+	ERR_ARGUMENTS             = 7
 	resource                  = "https://management.core.windows.net/" //TODO: make it a parameter for other clouds
 )
 
 var (
-	applicationID   = flag.String("applicationid", "", "service principal's application id")
-	tenantID        = flag.String("tenantid", "", "service principal's tenant id")
-	certificate     = flag.String("certificate", "", "full path to the certificate, pfx-formatted, containing the certificate and private key to be used in the authenticaton process")
-	pfxPassword     = flag.String("pfxpassword", "", "optional, pfx file password, it defaults to empty string")
-	tokenFileOutput = flag.String("tokenfileoutput", "", "full filename of the generated token")
-	cmdlineversion  = flag.Bool("version", false, "shows current tool version")
-	sniauth         = flag.Bool("usesniauth", false, "uses sn+i authentication option")
-	exitCode        = 0
-	version         = "1.0.0"
-	stdout          = log.New(os.Stdout, "", log.LstdFlags)
-	stderr          = log.New(os.Stderr, "", log.LstdFlags)
+	applicationID      = flag.String("applicationid", "", "service principal's application id")
+	tenantID           = flag.String("tenantid", "", "service principal's tenant id")
+	certificateFile    = flag.String("certificate", "", "full path to the certificate, pfx-formatted, containing the certificate and private key to be used in the authenticaton process")
+	pfxPassword        = flag.String("pfxpassword", "", "optional, pfx file password, it defaults to empty string")
+	tokenFileOutput    = flag.String("tokenfileoutput", "", "full filename of the generated token")
+	cmdLineVersion     = flag.Bool("version", false, "shows current tool version")
+	sniAuth            = flag.Bool("usesniauth", false, "uses sn+i authentication option")
+	useManagedIdentity = flag.Bool("usemanagedidentity", false, "use managed identity for authentication")
+	exitCode           = 0
+	version            = "1.1.0"
+	stdout             = log.New(os.Stdout, "", log.LstdFlags)
+	stderr             = log.New(os.Stderr, "", log.LstdFlags)
 )
 
 func exit(exitCode int) {
@@ -83,50 +86,76 @@ func main() {
 	}
 
 	// Checks if version output is needed
-	if *cmdlineversion {
+	if *cmdLineVersion {
 		fmt.Println(version)
 		exitCode = 0
 		return
 	}
 
-	// Checking if cert file exists
-	utils.ConsoleOutput("Checking if certificate file exists...", stdout)
-	if _, err := os.Stat(*certificate); err != nil {
-		utils.ConsoleOutput(fmt.Sprintf("<error> certificate file %v, not found: %v", *certificate, err), stderr)
-		exitCode = ERR_CERTIFICATE_NOT_FOUND
+	// Checks if useManagedIdentity is set while using certificate arguments
+	if *useManagedIdentity && (*sniAuth || *certificateFile != "" || *pfxPassword != "") {
+		utils.ConsoleOutput("<error> cannot use certificate arguments while using -usemanagedidentity", stderr)
+		exitCode = 7
 		return
 	}
 
-	// Read the certificate file
-	utils.ConsoleOutput("Reading the certificate file...", stdout)
-	certData, err := os.ReadFile(*certificate)
-	if err != nil {
-		utils.ConsoleOutput(fmt.Sprintf("<error> failed to read the certificate file (%s): %v", *certificate, err), stderr)
-		exitCode = ERR_CERTIFICATE_NOT_FOUND
-		return
-	}
-
-	// Get the certificate and private key from pfx file
-	utils.ConsoleOutput("Decoding the PFX to get the certificate and private key...", stdout)
-	rsaPrivateKey, certificate, err := decodePFX(certData, *pfxPassword)
-	if err != nil {
-		utils.ConsoleOutput(fmt.Sprintf("failed to decode pkcs12 certificate while creating service principal token: %v", err), stderr)
-		return
-	}
-
-	// Get the NewClientCertificateCredential
-	utils.ConsoleOutput("Creating NewClientCertificateCredential...", stdout)
-
+	var err error
+	var rsaPrivateKey interface{}
+	var certificate *x509.Certificate
 	clientCredentialOptions := azidentity.ClientCertificateCredentialOptions{}
-	if *sniauth {
-		clientCredentialOptions.SendCertificateChain = true
+
+	// Checking if cert file exists
+	if !*useManagedIdentity {
+		utils.ConsoleOutput("Checking if certificate file exists...", stdout)
+		if _, err := os.Stat(*certificateFile); err != nil {
+			utils.ConsoleOutput(fmt.Sprintf("<error> certificate file %v, not found: %v", *certificateFile, err), stderr)
+			exitCode = ERR_CERTIFICATE_NOT_FOUND
+			return
+		}
+
+		// Read the certificate file
+		utils.ConsoleOutput("Reading the certificate file...", stdout)
+		certData, err := os.ReadFile(*certificateFile)
+		if err != nil {
+			utils.ConsoleOutput(fmt.Sprintf("<error> failed to read the certificate file (%s): %v", *certificateFile, err), stderr)
+			exitCode = ERR_CERTIFICATE_NOT_FOUND
+			return
+		}
+
+		// Get the certificate and private key from pfx file
+		utils.ConsoleOutput("Decoding the PFX to get the certificate and private key...", stdout)
+		rsaPrivateKey, certificate, err = decodePFX(certData, *pfxPassword)
+		if err != nil {
+			utils.ConsoleOutput(fmt.Sprintf("failed to decode pkcs12 certificate while creating service principal token: %v", err), stderr)
+			return
+		}
+
+		// Get the NewClientCertificateCredential
+		utils.ConsoleOutput("Creating NewClientCertificateCredential...", stdout)
+
+		if *sniAuth {
+			clientCredentialOptions.SendCertificateChain = true
+		}
 	}
 
-	cred, err := azidentity.NewClientCertificateCredential(*tenantID, *applicationID, []*x509.Certificate{certificate}, rsaPrivateKey.(*rsa.PrivateKey), &clientCredentialOptions)
-	if err != nil {
-		utils.ConsoleOutput(fmt.Sprintf("<error> an error occurred creating ClientCertificateCredential: %v", err), stderr)
-		exitCode = ERR_AUTH_CONFIG
-		return
+	var cred azcore.TokenCredential
+
+	if *useManagedIdentity {
+		// Create a credential using Managed Identity
+		cred, err = azidentity.NewManagedIdentityCredential(nil)
+		if err != nil {
+			utils.ConsoleOutput(fmt.Sprintf("<error> an error occurred creating ManagedIdentityCredential: %v", err), stderr)
+			exitCode = ERR_AUTH_CONFIG
+			return
+		}
+	} else {
+		// Create the credential based on certificate
+		cred, err = azidentity.NewClientCertificateCredential(*tenantID, *applicationID, []*x509.Certificate{certificate}, rsaPrivateKey.(*rsa.PrivateKey), &clientCredentialOptions)
+		if err != nil {
+			utils.ConsoleOutput(fmt.Sprintf("<error> an error occurred creating ClientCertificateCredential: %v", err), stderr)
+			exitCode = ERR_AUTH_CONFIG
+			return
+		}
 	}
 
 	// Get the token
